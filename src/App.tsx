924c44b6cafaa80bafc433cf0eac41fbc657b9ef
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2022-11-17 00:16:42 -0300

    Migrate to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 
-import { Home } from './pages/home'
+import { Home } from './pages/home/index'
 import { Login } from './pages/login'
 import { Feed } from './pages/feed'
-
 
 import {
   BrowserRouter as Router,
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,8 @@
 
 import {ButtonContainer} from './styles'
+import {IButton} from './types';
 
-export const Button = ({title,variant = 'primary', onClick}) => {
+export const Button = ({title,variant = 'primary', onClick}: IButton) => {
     return (
 
         <ButtonContainer variant={variant} onClick={onClick}>            
